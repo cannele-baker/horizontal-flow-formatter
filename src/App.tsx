1c3777b1585d82dce1e059507118dac757c87a61
cannele-baker <44050587+cannele-baker@users.noreply.github.com>
2022-11-23 17:29:18 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Hello from "./hello";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Hello name="Horizontal Flow Diagram" />
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
